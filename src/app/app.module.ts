17aca06d5a69f0109b42320862a964e8cb1065e9
Sara3108 <51201757+Sara3108@users.noreply.github.com>
2019-06-01 00:02:54 +0200

    Register Component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,19 +7,13 @@ import { NavComponent } from './Components/nav/nav.component';
 import { FooterComponent } from './Components/footer/footer.component';
 import { LoginComponent } from './Components/login/login.component';
 import { RouterModule } from '@angular/router';
-import {MatFormFieldModule} from '@angular/material/form-field';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
-import {MatInputModule} from '@angular/material/input';
-import {NoopAnimationsModule, BrowserAnimationsModule} from '@angular/platform-browser/animations';
-import {MatButtonModule} from '@angular/material/button';
-import {MatIconModule} from '@angular/material/icon';
 import { RegisterComponent } from './Components/register/register.component';
-import {MatSelectModule} from '@angular/material/select';
-import {MatTabsModule} from '@angular/material/tabs';
 import { LocationPageComponent } from './Components/location-page/location-page.component';
 import { AuthLoginService } from './services/auth-login.service';
 import { HttpClientModule } from '@angular/common/http';
 import { HomeFiltrationContainerComponent } from './Components/home-filtration-container/home-filtration-container.component'; 
+import { MatComponentsModule } from './mat-components/mat-components.module';
 // import { HttpModule } from '@angular/common/http';
 
 @NgModule({
@@ -41,17 +35,11 @@ import { HomeFiltrationContainerComponent } from './Components/home-filtration-c
       {path:'register', component:RegisterComponent},
       {path:'location', component:LocationPageComponent}
     ]),
-    MatFormFieldModule,
-    MatInputModule,
+
     FormsModule,
     ReactiveFormsModule,
-    NoopAnimationsModule,
-    MatButtonModule,
-    MatIconModule,
-    MatSelectModule,
-    BrowserAnimationsModule,
     HttpClientModule,
-    MatTabsModule
+    MatComponentsModule
 
     
   ],
